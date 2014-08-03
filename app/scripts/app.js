18abe5927000f48d211043a0b4c34791756129af
laurentgrangeau <laurent.grangeau@gmail.com>
2014-08-03 22:12:33 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rxstockJsApp
+ * @description
+ * # rxstockJsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rxstockJsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rxstockJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rxstockJsApp
+ */
+angular.module('rxstockJsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
